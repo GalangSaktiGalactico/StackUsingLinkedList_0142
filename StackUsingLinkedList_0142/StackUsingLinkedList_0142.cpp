4e75f2311792214304cd3ae136fcaca701a449ff
Galang Sakti G <wadepakmen97@gmail.com>
2024-05-10 10:11:41 +0700

    void pop

--- StackUsingLinkedList_0142/StackUsingLinkedList_0142.cpp
+++ StackUsingLinkedList_0142/StackUsingLinkedList_0142.cpp
@@ -29,4 +29,12 @@ public:
 		return value;
 	}
 
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is Empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
+	}
+
 };
